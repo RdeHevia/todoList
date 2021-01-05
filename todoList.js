54d467c0fc092c3bedf364d9c81b080ceab48504
Raul de Hevia <raul.dehevia@gmail.com>
2021-01-05 12:48:56 -0800

    v2

--- todoList.js
+++ todoList.js
@@ -99,4 +99,43 @@ class TodoList {
     }, header);
     return displayText;
   }
+
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    let filteredTodos = new TodoList(this.title);
+    this.forEach(todo => {
+      if (callback(todo)) {
+        filteredTodos.add(todo);
+      }
+    });
+
+    return filteredTodos;
+  }
 }
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+list.add(todo5);
+list.add(todo6);
+todo1.markDone();
+todo5.markDone();
+
+console.log(list);
+console.log(list.first());
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
+let test = list.filter(todo => todo.isDone()).first();
+console.log(test);
