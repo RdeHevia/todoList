648aa088363678fb4aff9d8ac5c4e3aa454cf70f
Raul de Hevia <raul.dehevia@gmail.com>
2021-01-22 09:22:46 -0800

    v1

--- todoList.js
+++ todoList.js
@@ -1,33 +1,4 @@
-class Todo {
-  static DONE_MARKER = 'X';
-  static UNDONE_MARKER = ' ';
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-  // polymorphism (method override of String.prototype.toString)
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
+const Todo = require("./todo");
 
 class TodoList {
   constructor(title) {
@@ -146,3 +117,5 @@ class TodoList {
     return filteredTodos;
   }
 }
+
+module.exports = TodoList;
